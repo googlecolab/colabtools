ec7e0382aab07e72666e6da1f07395f97ec978f1
Mark Sandler <sandler@google.com>
2017-11-30 10:00:45 -0800

    Moves javascript buliding into standaone colab.output._js_builder
    RELNOTES: n/a
    PiperOrigin-RevId: 177468884

---
+++ google/colab/output/_publish.py
@@ -0,0 +1,45 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Provides a bunch of shortcuts to display popular types of content.
+
+This module is nothing by a thin wrapper over IPython.display but it allows
+shorter and cleaner code when all we want is to publish common content types.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import hashlib
+
+from IPython import display
+
+
+def html(content):
+  """Publishes given html content into the output."""
+  display.display(display.HTML(content))
+
+
+def css(content):
+  """Publishes css content."""
+  html('<style>' + content + '</style>')
+
+
+def javascript(content=None, script_id=None):
+  """Publishes javascript content into the output."""
+  if content.find('sourceURL=') < 0 or script_id:
+    if not script_id:
+      script_id = 'js_' + hashlib.md5(content.encode('utf8')).hexdigest()[:10]
+    content += '\n//# sourceURL=%s' % script_id
+  display.display(display.Javascript(content))
