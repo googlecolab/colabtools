b84e5eaded3c9968ce6ecb958705beb54bbe7909
Craig Citro <craigcitro@google.com>
2018-04-05 09:37:19 -0700

    Add some python3 trove classifiers.
    PiperOrigin-RevId: 191753455

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ setup(
     classifiers=(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX',
