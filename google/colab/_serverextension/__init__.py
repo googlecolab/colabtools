e7d2614ec2f2e4675772b6b4e756f6c270f8728e
Google Colaboratory Team <colaboratory-team@google.com>
2018-08-22 09:47:49 -0700

    Automated g4 rollback of changelist 209725608.
    *** Reason for rollback ***
    
    Breaks tests:
    
    https://travis-ci.org/googlecolab/colabtools/builds
    
    *** Original change description ***
    
    Speed up /colab/resources session memory tracking.
    
    Change internal and external memory handlers to use "ps -q pid,pid,pid" instead of "ps --ppid ppid". Get the list of pids from the IPython kernel manager, passed in to each kernel memory handler on initialization.
    
    Why? Before, the handler took ~hundreds of milliseconds to execute internally. The memory handler blocks code execution and session creation (though it is not blocked by code execution or session creation), so some optimization will...
    
    ***
    
    PiperOrigin-RevId: 209780619

--- google/colab/_serverextension/__init__.py
+++ google/colab/_serverextension/__init__.py
@@ -41,8 +41,6 @@ def load_jupyter_server_extension(nb_server_app):
   monitor_relative_path = '/api/colab/resources'
 
   app.add_handlers('.*$', [
-      (url_maker(monitor_relative_path), _handlers.ResourceUsageHandler, {
-          'kernel_manager': app.settings['kernel_manager']
-      }),
+      (url_maker(monitor_relative_path), _handlers.ResourceUsageHandler),
   ])
   nb_server_app.log.info('google.colab serverextension initialized.')
