70af4c7b9aef67eabda34dd2a84ff28871fbccb2
Google Colaboratory Team <colaboratory-team@google.com>
2018-10-11 08:53:59 -0700

    Internal update.
    PiperOrigin-RevId: 216703516

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ from setuptools import find_packages
 from setuptools import setup
 
 DEPENDENCIES = (
-    'google-auth',
+    'google-auth~=1.4.0',
     'ipykernel~=4.6.0',
     'ipython~=5.5.0',
-    'notebook>=5.0',
-    'six>=1.4.0',
-    'portpicker',
-    'requests',
-    'tornado>=4.5',
+    'notebook~=5.2.0',
+    'six~=1.11.0',
+    'portpicker~=1.2.0',
+    'requests~=2.18.0',
+    'tornado~=4.5.0',
 )
 
 setup(
