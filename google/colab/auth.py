10c2c834af296e6bb1a8de968b1b0db65405147e
Ami Fischman <fischman@google.com>
2020-06-24 09:33:27 -0700

    Internal update.
    PiperOrigin-RevId: 318079911

--- google/colab/auth.py
+++ google/colab/auth.py
@@ -151,12 +151,15 @@ def authenticate_user(clear_output=True):
       # If we've got a TPU attached, we want to run a TF operation to provide
       # our new credentials to the TPU for GCS operations.
       import tensorflow as tf  # pylint: disable=g-import-not-at-top
-      # TODO(b/135524562): Add support for authenticating TPUs in TF2.
       if tf.__version__.startswith('1'):
         with tf.compat.v1.Session('grpc://{}'.format(colab_tpu_addr)) as sess:
           with open(_get_adc_path()) as auth_info:
             tf.contrib.cloud.configure_gcs(
                 sess, credentials=_json.load(auth_info))
+      else:
+        # pytype: skip-file
+        import tensorflow_gcs_config as _tgc  # pylint: disable=g-import-not-at-top
+        _tgc.configure_gcs_from_colab_auth()
   if _check_adc():
     return
   raise _errors.AuthorizationError('Failed to fetch user credentials')
