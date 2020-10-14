2180752ed5333dd9dfaaad32268ad49032c4c490
Craig Citro <craigcitro@google.com>
2020-10-14 14:37:14 -0700

    Internal update.
    PiperOrigin-RevId: 337174714

--- google/colab/_serverextension/_resource_monitor.py
+++ google/colab/_serverextension/_resource_monitor.py
@@ -32,15 +32,8 @@ def get_gpu_usage():
   """
   global _GPU_EVER_USED
 
-  gpu_memory_path = '/var/colab/gpu-memory'
-  kernels = {}
   usage = 0
   limit = 0
-  if os.path.exists(gpu_memory_path):
-    with open(gpu_memory_path) as f:
-      reader = csv.DictReader(f.readlines(), delimiter=' ')
-      for row in reader:
-        kernels[row['kernel_id']] = int(row['gpu_mem(MiB)']) * 1024 * 1024
   try:
     ns = _serverextension._subprocess_check_output([  # pylint: disable=protected-access
         '/usr/bin/timeout', '-sKILL', '1s', 'nvidia-smi',
@@ -57,17 +50,12 @@ def get_gpu_usage():
     limit = int(row[1]) * 1024 * 1024
 
   if 'COLAB_FAKE_GPU_RESOURCES' in os.environ:
-    usage, limit, kernels = 123, 456, {'abc-789': 789, 'xyz-987': 987}
+    usage, limit = 123, 456
 
   if usage:
     _GPU_EVER_USED = True
 
-  return {
-      'usage': usage,
-      'limit': limit,
-      'kernels': kernels,
-      'ever_used': _GPU_EVER_USED
-  }
+  return {'usage': usage, 'limit': limit, 'ever_used': _GPU_EVER_USED}
 
 
 def get_ram_usage(kernel_manager):
@@ -94,22 +82,25 @@ def get_ram_usage(kernel_manager):
   if line:
     limit = int(line[0].split()[1]) * 1024
   usage = limit - free
-  pids_to_kernel_ids = dict([
-      (str(kernel_manager.get_kernel(kernel_id).kernel.pid), kernel_id)
-      for kernel_id in kernel_manager.list_kernel_ids()
-  ])
-  kernels = {}
-  if pids_to_kernel_ids:
-    ps = _serverextension._subprocess_check_output([  # pylint: disable=protected-access
-        'ps', '-q', ','.join(pids_to_kernel_ids.keys()), '-wwo', 'pid rss',
-        '--no-header'
-    ]).decode('utf-8')
-    for proc in ps.split('\n')[:-1]:
-      proc = proc.strip().split(' ', 1)
-      if len(proc) != 2:
-        continue
-      kernels[pids_to_kernel_ids[proc[0]]] = int(proc[1]) * 1024
-  return {'usage': usage, 'limit': limit, 'kernels': kernels}
+  result = {'usage': usage, 'limit': limit}
+  if not os.path.exists('/var/colab/hostname'):
+    pids_to_kernel_ids = dict([
+        (str(kernel_manager.get_kernel(kernel_id).kernel.pid), kernel_id)
+        for kernel_id in kernel_manager.list_kernel_ids()
+    ])
+    if pids_to_kernel_ids:
+      kernels = {}
+      ps = _serverextension._subprocess_check_output([  # pylint: disable=protected-access
+          'ps', '-q', ','.join(pids_to_kernel_ids.keys()), '-wwo', 'pid rss',
+          '--no-header'
+      ]).decode('utf-8')
+      for proc in ps.split('\n')[:-1]:
+        proc = proc.strip().split(' ', 1)
+        if len(proc) != 2:
+          continue
+        kernels[pids_to_kernel_ids[proc[0]]] = int(proc[1]) * 1024
+      result['kernels'] = kernels
+  return result
 
 
 def get_disk_usage(path=None):
