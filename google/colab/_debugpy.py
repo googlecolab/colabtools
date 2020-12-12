b930c0cffd593060cddf410c9732037f7d5dfb27
Google Colaboratory Team <colaboratory-team@google.com>
2020-12-11 16:44:03 -0800

    Combined export:   - 347100486 by blois <blois@google.com>   - 347093068 by blois <blois@google.com>
    PiperOrigin-RevId: 347100486

--- google/colab/_debugpy.py
+++ google/colab/_debugpy.py
@@ -1,5 +1,6 @@
 """Tools to enable debugpy attachment to a process."""
 
+import threading
 import debugpy
 import portpicker
 
@@ -20,9 +21,40 @@ def enable_attach_async():
   # TODO(b/64941125): Consider moving this earlier to avoid impact on kernel
   # connect time.
   _dap_port = portpicker.pick_unused_port()
-  debugpy.listen(_dap_port)
 
-  # TODO(b/175143091): Investigate what is causing the slowdowns here.
-  debugpy.trace_this_thread(False)
+  main_thread = threading.current_thread()
+  # Prevent debugpy from tracing the current thread.
+  main_thread.pydev_do_not_trace = True
+
+  def attachment_entry():
+    # The client will retry the connection a few times to avoid the inherent
+    # raciness of this.
+    debugpy.listen(_dap_port)
+
+    # Debugger tracing isn't needed for just tracebacks, but if full debugging
+    # is needed then it needs to be re-enabled while debugging.
+    # We want to use `pydevd.stoptrace` but if this is called before we have
+    # connected to the debug adapter from the client then it'll send a
+    # terminate to the adapter and the adapter will auto-exit before we can
+    # connect to it. After the connection then it's OK to terminate since the
+    # adapter will not close while there are active connections.
+    threading.settrace(None)  # for all future threads
+    try:
+      # Stop debugpy from tracing newly created threads.
+      from _pydev_bundle import pydev_monkey  # pylint: disable=g-import-not-at-top
+      pydev_monkey.undo_patch_thread_modules()
+    except ModuleNotFoundError:
+      # _pydev_bundle may be vendored into either location.
+      from debugpy.third_party.pydevd._pydev_bundle import pydev_monkey  # pylint: disable=g-import-not-at-top
+      pydev_monkey.undo_patch_thread_modules()
+
+    # Clear the trace flag to allow fetching stack traces.
+    main_thread.pydev_do_not_trace = False
+
+  # debugpy.listen will spin up another process then start listening for
+  # connections from that process. This can take a second or so, but most of it
+  # is not by this process. Doing this on a separate thread reduces the impact
+  # on kernel initialization.
+  threading.Thread(target=attachment_entry).start()
 
   return _dap_port
