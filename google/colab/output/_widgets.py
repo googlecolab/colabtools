ed177945de76c714963bfb8563fe76773d4eef1c
Pete Blois <blois@google.com>
2021-10-05 17:03:29 -0700

    Internal update.
    PiperOrigin-RevId: 401106439

--- google/colab/output/_widgets.py
+++ google/colab/output/_widgets.py
@@ -16,7 +16,7 @@
 import IPython as _IPython
 
 _supported_widgets_versions = {
-    '5.0.0a': '3922baeab3631134',
+    '5.0.0a': 'e680a8b83b2ea152',
 }
 _default_version = '5.0.0a'
 _installed_url = None
