1a5133380c8059735c016e25e7b94c254c86aa30
Katlyn Edwards <katlyn@google.com>
2022-04-20 16:13:00 -0700

    Internal change
    PiperOrigin-RevId: 443220040

--- google/colab/data_table.py
+++ google/colab/data_table.py
@@ -47,7 +47,7 @@ __all__ = [
     'load_ipython_extension', 'unload_ipython_extension'
 ]
 
-_GVIZ_JS = 'https://ssl.gstatic.com/colaboratory/data_table/9962d06c2255908d/data_table.js'
+_GVIZ_JS = 'https://ssl.gstatic.com/colaboratory/data_table/f872b2c2305463fd/data_table.js'
 
 _DATA_TABLE_HELP_URL = 'https://colab.research.google.com/notebooks/data_table.ipynb'
 
