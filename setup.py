67577b9bfce84229941434bf6be5b6305780d970
Eric Johnson <ericdjohnson@google.com>
2022-11-22 13:48:00 -0800

    Internal change
    PiperOrigin-RevId: 490327290

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
     classifiers=(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX',
