8ab2ca3d2ee11b59572d9a189c36357b35a3115a
Google Colaboratory Team <colaboratory-team@google.com>
2023-03-02 17:11:54 -0800

    Internal change
    PiperOrigin-RevId: 513680739

--- google/colab/data_table.py
+++ google/colab/data_table.py
@@ -45,7 +45,7 @@ __all__ = [
     'load_ipython_extension', 'unload_ipython_extension'
 ]
 
-_GVIZ_JS = 'https://ssl.gstatic.com/colaboratory/data_table/fb998edc550c7947/data_table.js'
+_GVIZ_JS = 'https://ssl.gstatic.com/colaboratory/data_table/99dac6621f6ae8c4/data_table.js'
 
 _DATA_TABLE_HELP_URL = 'https://colab.research.google.com/notebooks/data_table.ipynb'
 
