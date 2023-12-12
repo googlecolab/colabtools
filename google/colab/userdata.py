fdd9234d45e7c473a38368fd5c3d14f14e47503c
Google Colaboratory Team <colaboratory-team@google.com>
2023-12-11 16:47:41 -0800

    No public description
    PiperOrigin-RevId: 589993246

--- google/colab/userdata.py
+++ google/colab/userdata.py
@@ -1,13 +1,21 @@
 """API to access user secrets."""
 
 from google.colab import _message
+from google.colab import errors
 
 
 class NotebookAccessError(Exception):
-  """Exception thrown then the current notebook doesn't have access to the requested secret."""
+  """Exception thrown when the current notebook doesn't have access to the requested secret."""
 
   def __init__(self, key):
     super().__init__(f'Notebook does not have access to secret {key}')
+
+
+class SecretNotFoundError(errors.Error):
+  """Exception thrown when the requested secret is not found."""
+
+  def __init__(self, key):
+    super().__init__(f'Secret {key} does not exist.')
 
 
 def get(key):
@@ -22,11 +30,13 @@ def get(key):
   Raises:
     NotebookAccessError: If the notebook does not have access to the requested
     secret.
+    SecretNotFoundError: If the requested secret is not found.
   """
   resp = _message.blocking_request(
       'GetSecret', request={'key': key}, timeout_sec=None
   )
-  access = resp.get('access', False)
-  if not access:
+  if not resp.get('exists', False):
+    raise SecretNotFoundError(key)
+  if not resp.get('access', False):
     raise NotebookAccessError(key)
   return resp.get('payload', '')
