0d3697940db564a800db8d624834b510f743e8ab
Google Colaboratory Team <colaboratory-team@google.com>
2025-04-10 16:33:13 -0700

    No public description
    PiperOrigin-RevId: 746219143

--- google/colab/_import_hooks/_generativeai.py
+++ google/colab/_import_hooks/_generativeai.py
@@ -17,155 +17,164 @@ This will enable the IP geolocation restrictions for the Gemini API to be based
 on the location of the user instead of the runtime VM.
 """
 
-import imp  # pylint: disable=deprecated-module
+import importlib
 import logging
 import os
 import sys
 
+from google.colab._import_hooks._hook_injector import HookInjectorLoader
 
-class _GenerativeAIImportHook:
+
+class _GenerativeAIImportHook(importlib.abc.MetaPathFinder):
   """Enables the Gemini API client library to be customized upon import."""
 
-  def find_module(self, fullname, path=None):
+  def find_spec(self, fullname, path=None, target=None):
+    """Try to find a spec for google.generativeai and hook the module loader."""
     if fullname != 'google.generativeai':
       return None
-    self.module_info = imp.find_module(
-        fullname.split('.')[-1], list(path) if path else None
-    )
-    return self
 
-  def load_module(self, fullname):
-    """Loads google.generativeai normally and runs pre-initialization code.
+    def init_code_callback(module, previously_loaded):
+      """Loads google.generativeai normally and runs pre-initialization code.
 
-    It runs a background server that intercepts API requests and then proxies
-    the requests via the browser.
+      It runs a background server that intercepts API requests and then proxies
+      the requests via the browser.
 
-    Args:
-      fullname: fullname of the module
+      Args:
+        module: The module being loaded.
+        previously_loaded: Whether the module was previously loaded.
+      """
+      if not previously_loaded:
+        try:
+          import functools  # pylint:disable=g-import-not-at-top
+          import json  # pylint:disable=g-import-not-at-top
+          import google.api_core.exceptions  # pylint:disable=g-import-not-at-top
+          from google.colab import output  # pylint:disable=g-import-not-at-top
+          from google.colab.html import _background_server  # pylint:disable=g-import-not-at-top
+          import portpicker  # pylint:disable=g-import-not-at-top
+          import tornado.web  # pylint:disable=g-import-not-at-top
 
-    Returns:
-      A modified google.generativeai module.
-    """
-    previously_loaded = fullname in sys.modules
-    generativeai_module = imp.load_module(fullname, *self.module_info)
+          def fetch(request):
+            path = request.path
+            if request.query:
+              path = f'{path}?{request.query}'
+            method = request.method
+            headers = json.dumps(dict(request.headers))
+            body = (
+                repr(request.body.decode('utf-8')) if request.body else 'null'
+            )
+            return output.eval_js("""
+              (async () => {{
+                // The User-Agent header causes CORS errors in Firefox.
+                const headers = {headers};
+                delete headers["User-Agent"];
+                const response = await fetch(new URL('{path}', 'https://generativelanguage.googleapis.com'), {{
+                            method: '{method}',
+                            body: {body},
+                            headers,
+                          }});
+                const json = await response.json();
+                return json;
+              }})()
+          """.format(path=path, method=method, headers=headers, body=body))
 
-    if not previously_loaded:
-      try:
-        import functools  # pylint:disable=g-import-not-at-top
-        import json  # pylint:disable=g-import-not-at-top
-        import google.api_core.exceptions  # pylint:disable=g-import-not-at-top
-        from google.colab import output  # pylint:disable=g-import-not-at-top
-        from google.colab.html import _background_server  # pylint:disable=g-import-not-at-top
-        import portpicker  # pylint:disable=g-import-not-at-top
-        import tornado.web  # pylint:disable=g-import-not-at-top
+          class _Redirector(tornado.web.RequestHandler):
+            """Redirects API requests to the browser."""
 
-        def fetch(request):
-          path = request.path
-          if request.query:
-            path = f'{path}?{request.query}'
-          method = request.method
-          headers = json.dumps(dict(request.headers))
-          body = repr(request.body.decode('utf-8')) if request.body else 'null'
-          return output.eval_js("""
-            (async () => {{
-              // The User-Agent header causes CORS errors in Firefox.
-              const headers = {headers};
-              delete headers["User-Agent"];
-              const response = await fetch(new URL('{path}', 'https://generativelanguage.googleapis.com'), {{
-                          method: '{method}',
-                          body: {body},
-                          headers,
-                        }});
-              const json = await response.json();
-              return json;
-            }})()
-        """.format(path=path, method=method, headers=headers, body=body))
+            async def get(self):
+              await self._handle_request()
 
-        class _Redirector(tornado.web.RequestHandler):
-          """Redirects API requests to the browser."""
+            async def head(self):
+              await self._handle_request()
 
-          async def get(self):
-            await self._handle_request()
+            async def post(self):
+              await self._handle_request()
 
-          async def head(self):
-            await self._handle_request()
+            async def delete(self):
+              await self._handle_request()
 
-          async def post(self):
-            await self._handle_request()
+            async def patch(self):
+              await self._handle_request()
 
-          async def delete(self):
-            await self._handle_request()
+            async def put(self):
+              await self._handle_request()
 
-          async def patch(self):
-            await self._handle_request()
+            async def options(self):
+              await self._handle_request()
 
-          async def put(self):
-            await self._handle_request()
+            async def _handle_request(self):
+              try:
+                result = fetch(self.request)
+                if isinstance(result, dict) and 'error' in result:
+                  self.set_status(int(result['error']['code']))
+                  self.write(result['error']['message'])
+                  return
+                self.write(json.dumps(result))
+              except Exception as e:  # pylint:disable=broad-except
+                self.set_status(500)
+                self.write(str(e))
 
-          async def options(self):
-            await self._handle_request()
+          class _Proxy(_background_server._BackgroundServer):  # pylint: disable=protected-access
+            """Background server that intercepts API requests and then proxies the requests via the browser."""
 
-          async def _handle_request(self):
-            try:
-              result = fetch(self.request)
-              if isinstance(result, dict) and 'error' in result:
-                self.set_status(int(result['error']['code']))
-                self.write(result['error']['message'])
-                return
-              self.write(json.dumps(result))
-            except Exception as e:  # pylint:disable=broad-except
-              self.set_status(500)
-              self.write(str(e))
+            def __init__(self):
+              app = tornado.web.Application([
+                  (r'.*', _Redirector),
+              ])
+              super().__init__(app)
 
-        class _Proxy(_background_server._BackgroundServer):  # pylint: disable=protected-access
-          """Background server that intercepts API requests and then proxies the requests via the browser."""
+            def create(self, port):
+              if self._server_thread is None:
+                self.start(port=port)
 
-          def __init__(self):
-            app = tornado.web.Application([
-                (r'.*', _Redirector),
-            ])
-            super().__init__(app)
+          port = portpicker.pick_unused_port()
 
-          def create(self, port):
-            if self._server_thread is None:
-              self.start(port=port)
+          @functools.cache
+          def start():
+            p = _Proxy()
+            p.create(port=port)
+            return p
 
-        port = portpicker.pick_unused_port()
+          start()
 
-        @functools.cache
-        def start():
-          p = _Proxy()
-          p.create(port=port)
-          return p
-
-        start()
-
-        api_endpoint = f'http://localhost:{port}'
-        orig_configure = generativeai_module.configure
-        generativeai_module.configure = functools.partial(
-            orig_configure,
-            transport='rest',
-            client_options={'api_endpoint': api_endpoint},
-        )
-
-        # Change error messages to use the generative language API endpoint
-        # instead of the proxy endpoint.
-        orig_from_http_response = google.api_core.exceptions.from_http_response
-
-        @functools.wraps(orig_from_http_response)
-        def new_from_http_response(*args, **kwargs):
-          error = orig_from_http_response(*args, **kwargs)
-          error.message = error.message.replace(
-              api_endpoint, 'https://generativelanguage.googleapis.com'
+          api_endpoint = f'http://localhost:{port}'
+          orig_configure = module.configure
+          module.configure = functools.partial(
+              orig_configure,
+              transport='rest',
+              client_options={'api_endpoint': api_endpoint},
           )
-          return error
 
-        google.api_core.exceptions.from_http_response = new_from_http_response
-      except:  # pylint: disable=bare-except
-        logging.exception('Error customizing google.generativeai.')
-        os.environ['COLAB_GENERATIVEAI_IMPORT_HOOK_EXCEPTION'] = '1'
+          # Change error messages to use the generative language API endpoint
+          # instead of the proxy endpoint.
+          orig_from_http_response = (
+              google.api_core.exceptions.from_http_response
+          )
 
-    return generativeai_module
+          @functools.wraps(orig_from_http_response)
+          def new_from_http_response(*args, **kwargs):
+            error = orig_from_http_response(*args, **kwargs)
+            error.message = error.message.replace(
+                api_endpoint, 'https://generativelanguage.googleapis.com'
+            )
+            return error
+
+          google.api_core.exceptions.from_http_response = new_from_http_response
+        except:  # pylint: disable=bare-except
+          logging.exception('Error customizing google.generativeai.')
+          os.environ['COLAB_GENERATIVEAI_IMPORT_HOOK_EXCEPTION'] = '1'
+
+    loader = HookInjectorLoader(
+        fullname,
+        path,
+        target,
+        type(self),
+        init_code_callback,
+    )
+    # If the module can't be found returning a loader will cause the import to
+    # succeed but with an empty module. Avoid that case by returning None.
+    if not loader.find_spec():
+      return None
+    return importlib.util.spec_from_loader(fullname, loader)
 
 
 def _register_hook():
